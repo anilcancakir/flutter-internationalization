10067c2c7e49044eef0271f276c11c7ba7e53e57
Anılcan Çakır <anillcan.cakir@gmail.com>
2018-03-30 18:51:15 +0300

    Internationalization imported.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,74 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() => runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class DemoLocalizations {
+  DemoLocalizations(this.locale);
+
+  final Locale locale;
+
+  static DemoLocalizations of(BuildContext context) {
+    return Localizations.of<DemoLocalizations>(context, DemoLocalizations);
+  }
+
+  Map<String, String> _sentences;
+
+  Future<bool> load() async {
+    String data = await rootBundle.loadString('resources/lang/${this.locale.languageCode}.json');
+    this._sentences = json.decode(data);
+    return true;
+  }
+
+  String trans(String key) {
+    return this._sentences[key];
+  }
+}
+
+class DemoLocalizationsDelegate extends LocalizationsDelegate<DemoLocalizations> {
+  const DemoLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['tr', 'en'].contains(locale.languageCode);
+
+  @override
+  Future<DemoLocalizations> load(Locale locale) async {
+    DemoLocalizations localizations = new DemoLocalizations(locale);
+    await localizations.load();
+
+    print("Load ${locale.languageCode}");
+
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(DemoLocalizationsDelegate old) => false;
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      supportedLocales: [
+        const Locale('tr', 'TR'),
+        const Locale('en', 'US')
+      ],
+      localizationsDelegates: [
+        const DemoLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
+        for (Locale supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode || supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+
+        return supportedLocales.first;
+      },
       title: 'Flutter Internationalization',
       home: new MyPage(),
     );
@@ -17,8 +80,14 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Center(
-        child: new Text('Hello world!'),
+        child: new Text(
+          DemoLocalizations.of(context).trans('hello_world')
+        ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(new MyApp());
 }
